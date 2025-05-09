f2e8388fa2c3a36463e610519bfeab6a647d236e
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-09 18:36:47 -0500

    Adaptacion al async await del servicio getAll

--- services/eventos.service.js
+++ services/eventos.service.js
@@ -7,23 +7,21 @@ import pool from '../config/db.js'
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getAll
 
-export const getAll = function () {
+export const getAll = async function () {
  
     console.log("----------------------Service--------------------")
     
-    return new Promise( (resolve, reject) =>{
-        pool.query( 'select E.Id_Evento,E.Nombre_Evento, E.Fecha_Evento,C.Nom_Categoria, L.Nom_Local, L.Capacidad_Local from tb_evento E inner join tb_categoria C on E.Id_Cate = C.Id_Cate inner join tb_local L on E.Id_Local = L.Id_Local order by E.Id_Evento',
-                    (err, results, fields)=>{
-            console.log(results);
-            if(err){
-                reject(err)
-            } else{
-                resolve(results)
-            }
-        })
-    }
-    )
+    const [results,fields] = await pool.query( 
+            `select E.Id_Evento,E.Nombre_Evento, E.Fecha_Evento,C.Nom_Categoria, L.Nom_Local, 
+             L.Capacidad_Local 
+             from tb_evento E 
+             inner join tb_categoria C 
+             on E.Id_Cate = C.Id_Cate inner join tb_local L on E.Id_Local = L.Id_Local 
+             order by E.Id_Evento`)
+    console.log(results);
+    return results;
 }
+    
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getById
 
