9a72947231d85044c043ca45b4a076d04bed4f5f
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-17 20:57:21 -0500

    Adaptacion de metodos del controller pedidos al async await

--- controllers/pedidos.controller.js
+++ controllers/pedidos.controller.js
@@ -5,89 +5,79 @@ import * as spedidos from '../services/pedidos.service.js'
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getAll
 
-export const getAll = function (req, res) {
+export const getAll = async function (req, res) {
 
-    console.log("------------controller------------");
+    console.log("------------controller getAll pedidos------------");
 
-    spedidos.getAll()
-    .then( pedidos =>{
+    try {
+        let pedidos = await spedidos.getAll();
+        console.log("....despues de spedidos.getAll()");
+        res.json(pedidos || [] )
 
-            console.log("....despues de spedidos.getAll()");
+    } catch (error) {
+        res.status(500).json({"error":"Error obteniendo registros"});
+    }
 
-            res.json(pedidos || [] )
-
-    })
-    .catch(
-        err => {
-            res.status(500).json({"error":"Error obteniendo registros"});
-        }
-    )
-    /*res.send(arrEventoJson.arreglo)*/
 }
 
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getById
 
-export const getById = function (req, res) {
-    console.log("------------controller------------");
+export const getById =async function (req, res) {
+    console.log("------------controller getById pedidos------------");
 
-    spedidos.getById(req.params.id)
-    .then( objPedido =>{
+    try {
+        let pedido = await spedidos.getById(req.params.id);
+        console.log("....despues de spedidos.getById()");
+        res.json(pedido || [] )
 
-            console.log("....despues de spedidos.getById()");
+    } catch (error) {
+        res.status(500).json({"error":"Error obteniendo registros"});
+    }
 
-            res.json(objPedido || [] )
-
-    })
-    .catch(
-        err => {
-            res.status(500).json({"error":"Error obteniendo registros"});
-        }
-    )
-
-    /*res.send(arrEventoJson.arreglo[req.params.id])*/
 }
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método create
 
-export const create = function(req, res){
+export const create =async function(req, res){
 
-    const objPedido = req.body
-    console.log(objPedido)
-    spedidos.create(objPedido)
-    .then( idPedido =>{
+    console.log("------------controller create pedidos------------");
 
+    try {
+        
+        const objPedido = req.body
+        console.log(`Pedido a ingresar:`);
+        console.log(objPedido)
+
+        let idPedidoCreado=await spedidos.create(objPedido)
         console.log("....despues de spedidos.create()");
-        res.json({"Id de Pedido generado":idPedido});
+        res.json({"Id de Pedido generado":idPedidoCreado});
 
-        })
-    .catch(
-        err => {
+    } catch (error) {
         res.status(500).json({"error":"Error ingresando registros"});
     }
-)
 
-    //arrEventoJson.arreglo.push(objEvento)
-    /*res.send(objEvento)*/
 }
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método update
 
-export const update = function(req, res){
+export const update = async function(req, res){
 
-    const objPedido = req.body
-    console.log(objPedido)
-    spedidos.update(req.params.id,objPedido)
-    .then( NumRegistros =>{
+        console.log("------------controller update pedidos------------");
 
+    try {
+        
+        const objPedido = req.body
+        console.log(`Pedido a modificar:`);
+        console.log(objPedido)
+
+        let NumPedidosModificados=await spedidos.update(req.params.id,objPedido)
         console.log("....despues de spedidos.update()");
-        res.json({"NumeroRegistrosModificados":NumRegistros});
+        res.json({"N° registros modificados":NumPedidosModificados});
 
-        })
-    .catch(
-        err => {
-        res.status(500).json({"error":"Error actualizando registros"});
+    } catch (error) {
+        res.status(500).json({"error":"Error ingresando registros"});
     }
-)
+
 }
 
