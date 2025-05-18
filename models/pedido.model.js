b654203548955fbfca49d9d10bda0b10bddc8aeb
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-17 21:25:43 -0500

    Creacion de modelo de pedido y adaptacion de sus metodos y los metodos del servicio modelo

---
+++ models/pedido.model.js
@@ -0,0 +1,126 @@
+//Importar configuración de conexión a la BD
+import orm from '../config/sequelize.js'
+
+//Importación de módulos
+import { Sequelize,DataTypes, or, } from 'sequelize';
+
+
+
+// f para establecer la conexión a la base de datos
+
+export const connect = async function() {
+    try {
+        await orm.authenticate();
+        console.log("Conexión establecida");
+    } catch (error) {
+        console.error("Error al conectar:", error);
+    }
+}
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getAll
+
+export const getAll = async function () {
+ 
+    console.log("----------------------Model getAll pedidos--------------------")
+    const [results,fields] = await orm.query(
+        `select 
+            P.id_pedido,U.nom_usuario,U.correo_usuario,P.fecha_pedido,
+            P.monto_total_pedido,MP.nombre_mediopago,E.Nombre_Evento 
+        from tb_pedido P 
+        inner join tb_usuario U 
+         on P.id_usuario = U.id_usuario 
+        inner join tb_evento E 
+         on P.Id_Evento = E.Id_Evento 
+        inner join tb_mediopago MP 
+         on MP.id_mediopago = P.id_mediopago`,[])
+    
+    console.log(`Resultados de modelo GETALL pedidos`);
+    console.log(results);
+    return results
+
+}
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getById
+
+export const getById = async function (Id_Pedido) {
+ 
+    console.log("----------------------Model Listar por ID pedidos--------------------")
+    
+
+    const [results,fields] = await orm.query(
+        `select 
+            P.id_pedido,U.nom_usuario,U.correo_usuario,
+            P.fecha_pedido,P.monto_total_pedido,
+            MP.nombre_mediopago,
+            E.Nombre_Evento 
+        from tb_pedido P 
+        inner join tb_usuario U 
+         on P.id_usuario = U.id_usuario 
+        inner join tb_evento E 
+         on P.Id_Evento = E.Id_Evento 
+        inner join tb_mediopago MP
+         on MP.id_mediopago = P.id_mediopago 
+         where id_pedido = ?`,
+        {
+            replacements:[Id_Pedido]
+        }
+    )
+         
+
+    console.log(`Resultados de modelo GETBYID pedidos`);
+    console.log(`Id solicitado: ${Id_Pedido}`);
+    console.log(`Pedido (solo objeto): `);
+    console.log(results[0]);
+    console.log(`Pedido (array completo): `);
+    console.log(results);
+    return results[0]
+}
+
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método create
+
+export const create = async function (objPedido) {
+ 
+    
+    console.log("----------------------Modelo Insertar nuevo Pedido--------------------")
+    
+    const [results,fields] = await orm.query(
+        `INSERT INTO tb_pedido  (fecha_pedido,monto_total_pedido,id_mediopago, id_usuario, Id_Evento ) 
+                         VALUES (now(),?,?,?,?)`,
+        {
+            replacements: [objPedido.monto_total_pedido, objPedido.id_mediopago,objPedido.id_usuario,objPedido.Id_Evento]
+        }
+    )
+        
+             
+    console.log(`Resultados de servico CREATE pedidos`);
+    console.log(results);
+    console.log(`Obteniendo ID generado:`);
+    console.log(results);
+
+    return results;
+}
+
+// ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método update
+
+export const update = async function (Id_Pedido, objPedido) {
+ 
+    console.log("----------------------Modelo Modificar Pedido--------------------")
+
+    const [results,fields] = await orm.query( 
+        `update tb_pedido 
+            set monto_total_pedido = ?, id_mediopago= ?, id_usuario=?, Id_Evento = ? 
+        where id_pedido = ?`,
+        {
+            replacements:[objPedido.monto_total_pedido,objPedido.id_mediopago,objPedido.id_usuario,objPedido.Id_Evento,Id_Pedido]
+        }
+    )
+        
+
+    console.log(`Resultados de modelo:`);
+    console.log(results);
+    console.log(`Obteniendo n° filas afectadas:`);
+    console.log(results.affectedRows);
+
+    return results.affectedRows;
+}
--- services/pedidos.service.js
+++ services/pedidos.service.js
@@ -3,25 +3,23 @@
 
 import pool from '../config/db.js'
 
+//Importar el modelo de Pedidos (sequelize)
+
+import * as modelPedido from '../models/pedido.model.js';
+
+
 // Crear funciones
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método getAll
 
 export const getAll = async function () {
- 
-    console.log("----------------------Service getAll pedidos--------------------")
-    const [results,fields] = await pool.query(
-        `select 
-            P.id_pedido,U.nom_usuario,U.correo_usuario,P.fecha_pedido,
-            P.monto_total_pedido,MP.nombre_mediopago,E.Nombre_Evento 
-        from tb_pedido P 
-        inner join tb_usuario U 
-         on P.id_usuario = U.id_usuario 
-        inner join tb_evento E 
-         on P.Id_Evento = E.Id_Evento 
-        inner join tb_mediopago MP 
-         on MP.id_mediopago = P.id_mediopago`,[])
-    
+
+    console.log("----------------------Service--------------------")
+
+    //Llamado al método getAll del modelo de pedidos
+
+    const results = await modelPedido.getAll();
+
     console.log(`Resultados de servico GETALL pedidos`);
     console.log(results);
     return results
@@ -34,27 +32,15 @@ export const getById = async function (Id_Pedido) {
  
     console.log("----------------------Service Listar por ID pedidos--------------------")
     
+    //Llamado al método getById del modelo de pedidos
 
-    const [results,fields] = await pool.query(
-        `select 
-            P.id_pedido,U.nom_usuario,U.correo_usuario,
-            P.fecha_pedido,P.monto_total_pedido,
-            MP.nombre_mediopago,
-            E.Nombre_Evento 
-        from tb_pedido P 
-        inner join tb_usuario U 
-         on P.id_usuario = U.id_usuario 
-        inner join tb_evento E 
-         on P.Id_Evento = E.Id_Evento 
-        inner join tb_mediopago MP
-         on MP.id_mediopago = P.id_mediopago 
-         where id_pedido = ?`,[Id_Pedido])
+    const pedido = await modelPedido.getById(Id_Pedido);
 
     console.log(`Resultados de servico GETBYID pedidos`);
     console.log(`Id solicitado: ${Id_Pedido}`);
     console.log(`Pedido: `);
-    console.log(results[0]);
-    return results[0]
+    console.log(pedido);
+    return pedido
 }
 
 // ⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩⟨~⟩  Método create
@@ -64,18 +50,16 @@ export const create = async function (objPedido) {
     
     console.log("----------------------Service Insertar nuevo Pedido--------------------")
     
-    const [results,fields] = await pool.query(
-        `INSERT INTO tb_pedido  (fecha_pedido,monto_total_pedido,id_mediopago, id_usuario, Id_Evento ) 
-                         VALUES (now(),?,?,?,?)`,
-        
-        [objPedido.monto_total_pedido, objPedido.id_mediopago,objPedido.id_usuario,objPedido.Id_Evento])
+    //Llamado al método getById del modelo de pedidos
+
+    let idPedidoCreado = await modelPedido.create(objPedido);
+
              
     console.log(`Resultados de servico CREATE pedidos`);
-    console.log(results);
     console.log(`Obteniendo ID generado:`);
-    console.log(results.insertId);
+    console.log(idPedidoCreado);
 
-    return results.insertId;
+    return idPedidoCreado;
 }
 
 
@@ -85,18 +69,12 @@ export const update = async function (Id_Pedido, objPedido) {
  
     console.log("----------------------Service Modificar Pedido--------------------")
 
-    const [results,fields] = await pool.query( 
-        `update tb_pedido 
-            set monto_total_pedido = ?, id_mediopago= ?, id_usuario=?, Id_Evento = ? 
-        where id_pedido = ?`,
-            [objPedido.monto_total_pedido,objPedido.id_mediopago,objPedido.id_usuario,objPedido.Id_Evento,Id_Pedido])
-
+    let NumPedidosModificados = await modelPedido.update(Id_Pedido,objPedido);
 
     console.log(`Resultados de servico UPDATE pedidos`);
-    console.log(results);
     console.log(`Obteniendo n° filas afectadas:`);
-    console.log(results.affectedRows);
+    console.log(NumPedidosModificados);
 
-    return results.affectedRows;
+    return NumPedidosModificados;
 }
 
